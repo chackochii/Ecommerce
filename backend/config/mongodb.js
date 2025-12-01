d98fd3e32cd6c52bd263354ea5e3bd7efda82a14
chackochii <163020056+chackochii@users.noreply.github.com>
2025-12-01 22:52:57 +0530

    Refactor MongoDB connection logic
    Removed TLS options and simplified connection logic.

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,22 +1,13 @@
-import mongoose from "mongoose";
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      tls: false,                     // enable TLS
-      tlsAllowInvalidCertificates: false // Atlas has valid certs
-    });
-
-    mongoose.connection.on("connected", () => {
-      console.log("DB Connected");
-    });
-
-  } catch (err) {
-    console.error("MongoDB connection error:", err);
-    process.exit(1);
-  }
-};
-
+import mongoose from "mongoose"; 
+ 
+const connectDB = async () => { 
+ 
+    mongoose.connection.on('connected',() => { 
+        console.log("DB Connected"); 
+    }) 
+ 
+    await mongoose.connect(`${process.env.MONGODB_URI}`) 
+ 
+} 
+ 
 export default connectDB;
